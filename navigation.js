778e7bebc875d23db1f8900f313de564775a12c3
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-09-22 22:24:17 -0400

    configuring redux for app

--- navigation.js
+++ navigation.js
@@ -2,6 +2,10 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Home, RestaurantDetail } from "./screens";
+import { Provider as ReduxProvider } from "react-redux";
+import configureStore from "./redux/store";
+
+const store = configureStore();
 
 export default function RootNavigator() {
   const Stack = createStackNavigator();
@@ -10,11 +14,13 @@ export default function RootNavigator() {
   };
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-        <Stack.Screen component={Home} name="Home" />
-        <Stack.Screen component={RestaurantDetail} name="RestaurantDetail" />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+          <Stack.Screen component={Home} name="Home" />
+          <Stack.Screen component={RestaurantDetail} name="RestaurantDetail" />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ReduxProvider>
   );
 }
